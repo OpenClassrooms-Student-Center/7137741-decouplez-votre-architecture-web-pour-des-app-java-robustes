52ffc191c27d1ab76a9575be7693db74dcc7723a
lorraine@lotiste-pc.local <lorraine@lotiste-pc>
2021-03-30 09:43:38 +0200

    Spring Boot, Thymeleaf, index.html and logo

---
+++ src/main/java/com/airbusiness/airbusinessmvc/AirbusinessMvcApplication.java
@@ -0,0 +1,13 @@
+package com.airbusiness.airbusinessmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbusinessMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbusinessMvcApplication.class, args);
+	}
+
+}
